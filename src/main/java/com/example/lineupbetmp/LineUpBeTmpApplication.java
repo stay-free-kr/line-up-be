998968aaab62dd58e23929aece10e23f645b204f
jyong-99 <alexj99@etners.com>
2026-03-04 08:32:58 +0900

    init

---
+++ src/main/java/com/example/lineupbetmp/LineUpBeTmpApplication.java
@@ -0,0 +1,13 @@
+package com.example.lineupbetmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineUpBeTmpApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LineUpBeTmpApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/lineupbetmp/LineUpBeTmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lineupbetmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineUpBeTmpApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
